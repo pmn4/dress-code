cebf55251cfd88fec619a46642aca94cdfbdc4ac
Eric Weinstein <eweinstein@renttherunway.com>
2013-11-12 18:04:08 -0500

    we should at least pretend we'll write tests for all this

---
+++ spec/home_app_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'Home app' do
+  it 'should have tests' do
+  end
+end
+
---
+++ spec/mens_app_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'Mens app' do
+  it 'should have tests' do
+  end
+end
+
