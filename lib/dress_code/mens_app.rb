ea2d71020502c5b3f7bd00144a2e72d41ee5ad18
Eric Weinstein <eweinstein@renttherunway.com>
2013-11-12 23:56:35 -0500

    small formatting changes

--- lib/dress_code/mens_app.rb
+++ lib/dress_code/mens_app.rb
@@ -21,21 +21,25 @@ module DressCode
 		end
 
 		before do
-			content_type 'application/json'
+			content_type :json
 			@product_client = Gilt::Product.client('0b6c9ad5cbc67f6c1d7ff738f6e19d4a')
 			@sale_client = Gilt::Sale.client('0b6c9ad5cbc67f6c1d7ff738f6e19d4a')
 		end
 
 		get '/' do
-			request = @product_client.query :store => Gilt::Stores::MEN, :q => 'shirt', :color => ['blue','red']
+			request = @product_client.query :store => Gilt::Stores::MEN,
+                                      :q => 'shirt',
+                                      :color => %w(blue red)
 			response = request.perform
 			response.to_json
 		end
+
 		get '/categories' do
 			request = @product_client.categories
 			response = request.perform.parse
 			response.to_json
 		end
+
 		get '/sales' do
 			request = @sale_client.active_in_store :store => Gilt::Stores::MEN
 			response = request.perform.parse
@@ -43,3 +47,4 @@ module DressCode
 		end
 	end
 end
+
