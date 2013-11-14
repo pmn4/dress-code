fa27135c2bdefd4e6c74fa16c44fba93f2425c11
Eric Weinstein <eweinstein@renttherunway.com>
2013-11-14 16:41:13 -0500

    mongoid config

--- boot.rb
+++ boot.rb
@@ -1,8 +1,13 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler'
+require 'mongoid'
 
 Bundler.setup
 Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
 Dir["./lib/**/*.rb"].each { |f| require f }
+
+# Mongoid config
+Mongoid.load!('config/mongoid.yml')
+
