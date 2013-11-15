40d9986da175ce918b3c94a16e4c19f6fd906467
Eric Weinstein <eweinstein@renttherunway.com>
2013-11-15 18:44:18 -0500

    use underscore template for event summary view

--- public/js/views/code.js
+++ public/js/views/code.js
@@ -10,11 +10,6 @@ var CodeView = Backbone.View.extend({
   }
 , render: function() {
     $(this.el).html(this.template());
-     var eventItem = JSON.parse(this.collection['models'][0]['attributes']['event_data']);
-     _.each(eventItem, function(element, index, list) {
-       $('#summary').append('<li>' + element + '</li>');
-     });
-
     return this;
   }
 });
